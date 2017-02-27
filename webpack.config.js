b6c1451f3a63c3985a3445d93fbed4d6067946e0
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2017-02-27 14:36:14 +0100

    Clean webpack config file
    Signed-off-by: Jean-Baptiste Louazel <jb.louazel@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
+const path = require('path');
 const webpack = require('webpack');
-const {
-    version
-} = require('./package.json');
+const { version } = require('./package.json');
 
 module.exports = {
     entry: './provider.js',
     output: {
-        library: [
-            'StreamrootHlsjs'
-        ],
+        library: 'StreamrootHlsjs',
         libraryTarget: 'umd',
-        path: __dirname + '/dist',
+        path: path.resolve(__dirname, 'dist'),
         filename: 'clappr0-hlsjs-provider.js'
     },
     module: {
-        noParse: /node_modules\/streamroot-p2p\/p2p.js/,
+        noParse:  /node_modules\/hlsjs-p2p-bundle\/hlsjs-p2p-bundle.js/,
         loaders: [{
             exclude: /node_modules/,
             loader: 'babel-loader',
             test: /\.js$/
         }]
+    },
+    resolve: {
+      modules: [
+        'node_modules',
+      ],
     },
     externals: {
         clappr: {
@@ -34,7 +36,6 @@ module.exports = {
             __NAME__: 'streamroot_playback',
             __VERSION__: JSON.stringify(`v${version}`)
         }),
-        new webpack.optimize.OccurrenceOrderPlugin(true),
         new webpack.optimize.UglifyJsPlugin()
     ]
 };
